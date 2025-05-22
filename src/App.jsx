04d32469694555d602994818eaa6654f4d7e9b59
luigi-giordano <luigi.giordano1992@gmail.com>
2025-05-22 12:01:59 +0200

    [add css]

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useEffect } from "react"
 
 function App() {
+
+  const API_URL = "http://localhost:3333";
+
+  const [politici, setPolitici] = useState([]);
+
+  useEffect(() => {
+
+    fetch(`${API_URL}/politicians`)
+      .then(res => res.json())
+      .then(data => setPolitici(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  console.log(politici);
+
   return (
     <div>
-
+      <h1>Lista Politici</h1>
+      <div className="politicians-list">
+        {politici.map(p => (
+          <div className="card" key={p.id}>
+            <img src={p.image} alt={p.name} />
+            <h2>{p.name}</h2>
+            <p><strong>Posizione:</strong>{p.position}</p>
+            <p>{p.biography}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
